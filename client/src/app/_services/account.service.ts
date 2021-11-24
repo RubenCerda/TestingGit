5e5a58253eb4306c8b865e53d0673e31e2dc85a0
RubenCerda <rcerda@devbot.com>
2021-11-23 21:22:24 -0600

    End of Section 9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, JsonpClientBackend } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_modules/user';
 
 @Injectable({
@@ -9,7 +10,7 @@ import { User } from '../_modules/user';
 })
 export class AccountService {
 
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
   
@@ -34,7 +35,7 @@ export class AccountService {
         if(user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
-        }
+        } 
         return user;
       })
     );
